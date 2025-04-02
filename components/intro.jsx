3fe930615066de77e1ca62a078604cb76b52e187
George-MichaelKo <148492919+George-MichaelKo@users.noreply.github.com>
2025-04-01 23:31:51 -0500

    intro.jsx

--- components/intro.jsx
+++ components/intro.jsx
@@ -1,18 +1,33 @@
 export default function Intro(){
-    return(
-        < div className="intro">
-        <h2>George-Michael Kondoun</h2>
-        <h3>Computer Science Major</h3>
-        <h4>George-Michael's Digital Card</h4>
-        <section className="button">
-        <button className="email-btn">
-    <i className="fas fa-envelope"></i> <span>Email</span>
-  </button>
-  <button className="link-btn">
-    <i className="fab fa-linkedin"></i> <span>LinkedIn</span>
-  </button>
-        </section>
+    const handleEmailClick = () => {
+        window.location.href = "mailto:georgemichaelm16@gmail.com";
+      };
+    
+      // Function to handle the LinkedIn link click
+      const handleLinkedInClick = () => {
+        window.location.href = "http://linkedin.com/in/george-michael-kondoun-67964b27a";
+      };
+    
+      return (
+        <div className="intro">
+          <h2>George-Michael Kondoun</h2>
+          <h3>Computer Science Major</h3>
+          <h4>George-Michael's Digital Card</h4>
+          <section className="button">
+            {/* Email Button with span and handleEmailClick function */}
+            <button className="email-btn" onClick={handleEmailClick}>
+              <i className="fas fa-envelope"></i>
+              <span>Email</span>
+            </button>
+    
+            {/* LinkedIn Button with handleLinkedInClick function */}
+            <button className="link-btn" onClick={handleLinkedInClick}>
+              <i className="fab fa-linkedin"></i>
+              <span>LinkedIn</span>
+            </button>
+          </section>
         </div>
     )
+
 
 }
